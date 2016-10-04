5c7bd72e4d50f93efdae3d82ad064bff56dcef2e
Chao Yang <chaoyang.nz@gmail.com>
2016-10-04 13:20:12 +0800

    done

--- src/main/java/_004_median_of_two_sorted_arrays/Solution.java
+++ src/main/java/_004_median_of_two_sorted_arrays/Solution.java
@@ -10,9 +10,9 @@ public class Solution {
         int n = nums2.length;
 
         if((m+n)%2 > 0) { //odd
-            return findKth(nums1, 0, m-1, nums2, 0, n-1, (m+n+1)/2);
+            return findKth(nums1, 0, nums2, 0, (m+n+1)/2);
         } else {
-            return (findKth(nums1, 0, m-1, nums2, 0, n-1, (m+n+1)/2) + findKth(nums1, 0, m-1, nums2, 0, n-1, (m+n+1)/2+1))/2.0;
+            return (findKth(nums1, 0, nums2, 0, (m+n+1)/2) + findKth(nums1, 0, nums2, 0, (m+n+1)/2+1))/2.0;
         }
     }
 
@@ -21,25 +21,24 @@ public class Solution {
      *
      * @param nums1
      * @param i1 arr1子数组的开始坐标
-     * @param j1 arr1子数组的结束坐标
      * @param nums2
      * @param i2 arr2子数组的开始坐标
-     * @param j2 arr2子数组的结束坐标
      * @param k 找到k个数比y小
      * @return
      */
-    private double findKth(int[] nums1, int i1, int j1, int[] nums2, int i2, int j2, int k) {
-        int m = j1-i1+1, n = j2-i2+1;
-        if(m > n) return findKth(nums2, i2, j2, nums1, i1, j1, k); // make first array always have smaller size
-        if(i1>j1) return nums2[i2+k-1];
+    private double findKth(int[] nums1, int i1, int[] nums2, int i2, int k) {
+        int m = nums1.length-i1, n = nums2.length-i2;
+        if(m > n) return findKth(nums2, i2, nums1, i1, k); // make first array always have smaller size
+
+        if(i1 == nums1.length) return nums2[i2+k-1];
         if(k == 1) return Math.min(nums1[i1], nums2[i2]);
 
-        int pa = Math.min(k >> 1, m), pb = k - pa;
-        if (nums1[i1+pa-1] < nums2[i2+pb-1])
-            return findKth(nums1, i1+pa, j1, nums2, i2, j2, k - pa);
-        else if (nums1[i1+pa-1] > nums2[i2+pb-1])
-            return findKth(nums1, i1, j1, nums2, i2+pb, j2, k - pb);
+        int p1 = Math.min(k/2, m), p2 = k - p1;
+        if (nums1[i1+p1-1] < nums2[i2+p2-1])
+            return findKth(nums1, i1+p1, nums2, i2, k-p1);
+        else if (nums1[i1+p2-1] > nums2[i2+p2-1])
+            return findKth(nums1, i1, nums2, i2+p2, k-p2);
         else
-            return nums1[i1+pa-1];
+            return nums1[i1+p1-1];
     }
 }
