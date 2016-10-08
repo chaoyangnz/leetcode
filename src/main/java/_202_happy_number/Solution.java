e0d65251287324cac4bb75f6f843b2d1707aec8c
Chao Yang <chaoyang.nz@gmail.com>
2016-10-08 23:14:29 +0800

    done

--- src/main/java/_202_happy_number/Solution.java
+++ src/main/java/_202_happy_number/Solution.java
@@ -1,9 +1,44 @@
 
 package _202_happy_number;
 
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode.com/problems/happy-number
  */
 public class Solution {
+    public boolean isHappy(int n) {
+        boolean[] existing = new boolean[810];
+        int i = squareSum(n, null);
+        while (i != 1) {
+            if(existing[i]) return false; // duplicated
+            i = squareSum(i, existing);
+        }
 
+        return true;
+    }
+
+    private int squareSum(int n, boolean[] existing) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(3);
+        int sqrsum = 0;
+        int i = n;
+        while (i > 0) {
+            int digit = i % 10;
+            if (digit != 0) {
+                sqrsum += digit * digit;
+                queue.add(digit);
+            }
+            i /= 10;
+        }
+
+        if (existing!=null) {
+            int key = 0;
+            while (!queue.isEmpty()) {
+                key = key * 10 + queue.poll();
+            }
+            existing[key] = true;
+        }
+
+        return sqrsum;
+    }
 }
--- src/main/java/_202_happy_number/Tests.java
+++ src/main/java/_202_happy_number/Tests.java
@@ -8,7 +8,22 @@ public class Tests {
 
 	@Test
 	public void test1() {
+		System.out.println(Integer.MAX_VALUE);
+		int n = 19;
+		System.out.println(solution.isHappy(n));
+
+		n = 725083991;
+		System.out.println(solution.isHappy(n));
+
+		n = 1012321673;
+		System.out.println(solution.isHappy(n));
+
+		for(int i = 1; i < Integer.MAX_VALUE; ++i) {
+			if(solution.isHappy(i)) {
+//				System.out.println(i);
+			}
+
+		}
 
 	}
-	
 }
