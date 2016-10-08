935c43d9ad95d859a9bac04b9889be5e7136176b
Richard Yang <richard.yang@blackboard.com>
2016-10-08 18:21:46 +0800

    done

--- src/main/java/_204_count_primes/Solution.java
+++ src/main/java/_204_count_primes/Solution.java
@@ -1,9 +1,25 @@
 
 package _204_count_primes;
 
+import java.util.BitSet;
+
 /**
  * https://leetcode.com/problems/count-primes
  */
 public class Solution {
+    public int countPrimes(int n) {
+        if(n < 2) return 0;
 
+        BitSet composites = new BitSet(n); // initial all are primes
+        int count = 0; // the first prime is 2
+        for(int num = 2; num>= 0 && num < n; ++num) {
+            if(!composites.get(num)) { // is prime
+                count++;
+                for(int i=2; num*i >= 0 && num*i < n; i++) {
+                    composites.set(num*i);
+                }
+            }
+        }
+        return count;
+    }
 }
--- src/main/java/_204_count_primes/Tests.java
+++ src/main/java/_204_count_primes/Tests.java
@@ -1,6 +1,7 @@
 
 package _204_count_primes;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Tests {
@@ -8,7 +9,22 @@ public class Tests {
 
 	@Test
 	public void test1() {
+		int n = 3;
+		int count = solution.countPrimes(n);
+		Assert.assertEquals(1, count);
 
+		n = 5;
+		count = solution.countPrimes(n);
+		Assert.assertEquals(2, count);
+
+		n = 10;
+		count = solution.countPrimes(n);
+		Assert.assertEquals(4, count);
+
+		n = Integer.MAX_VALUE;
+		System.out.println(n);
+//		count = solution.countPrimes(n);
+//		Assert.assertEquals(105097564, count);
 	}
 	
 }
