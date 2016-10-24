3dcdd46ee2fa05554328c8311925f5bff3aa73b6
Richard Yang <richard.yang@blackboard.com>
2016-10-24 17:11:57 +0800

    done

---
+++ src/main/java/_141_linked_list_cycle/README.md
@@ -0,0 +1,10 @@
+# Problem
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+# Analysis
+
+使用快慢指针，如果有循环，必定会再次相遇
--- src/main/java/_141_linked_list_cycle/Solution.java
+++ src/main/java/_141_linked_list_cycle/Solution.java
@@ -5,5 +5,25 @@ package _141_linked_list_cycle;
  * https://leetcode.com/problems/linked-list-cycle
  */
 public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        do {
+            if(slow == null || fast == null || fast.next == null) return false;
 
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast);
+
+        return true;
+    }
+}
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
