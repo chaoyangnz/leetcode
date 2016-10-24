0aecb0aaaede706b329e2e6e85b7402549267c57
Richard Yang <richard.yang@blackboard.com>
2016-10-24 18:43:17 +0800

    done

--- src/main/java/_101_symmetric_tree/Solution.java
+++ src/main/java/_101_symmetric_tree/Solution.java
@@ -1,9 +1,54 @@
 
 package _101_symmetric_tree;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/symmetric-tree
  */
 public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
 
+    public boolean isSymmetricIterative(TreeNode root) {
+        if(root == null) return true;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.right);
+        stack.push(root.left);
+        while (!stack.empty()) {
+            TreeNode l = stack.pop();
+            TreeNode r = stack.pop();
+            if(l == null && r == null) continue;
+            if(l == null && r != null) return false;
+            if(l != null && r == null) return false;
+            if(l.val != r.val) return false;
+            // pair
+            stack.push(r.right);
+            stack.push(l.left);
+            // pair
+            stack.push(r.left);
+            stack.push(l.right);
+        }
+        return true;
+    }
+
+    private boolean isSymmetric(TreeNode l, TreeNode r) {
+        if(l == null && r == null) return true;
+        if(l == null && r != null) return false;
+        if(l != null && r == null) return false;
+        if(l.val != r.val) return false;
+
+        return isSymmetric(l.left, r.right) && isSymmetric(l.right, r.left);
+    }
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
--- src/main/java/_101_symmetric_tree/Tests.java
+++ src/main/java/_101_symmetric_tree/Tests.java
@@ -8,7 +8,21 @@ public class Tests {
 
 	@Test
 	public void test1() {
+		//[2,3,3,4,5,5]
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3l = new TreeNode(3);
+		TreeNode node3r = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5l = new TreeNode(5);
+		TreeNode node5r = new TreeNode(5);
 
+		node2.left = node3l;
+		node2.right = node3r;
+		node3l.left = node4;
+		node3l.right = node5l;
+		node3r.left = node5r;
+
+		System.out.println(solution.isSymmetricIterative(node2));
 	}
 	
 }
