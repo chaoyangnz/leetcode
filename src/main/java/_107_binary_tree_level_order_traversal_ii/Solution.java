2164f4e57d2de166bde8c4be681cd2df28d9501e
Richard Yang <richard.yang@blackboard.com>
2016-10-26 10:23:12 +0800

    done

--- src/main/java/_107_binary_tree_level_order_traversal_ii/Solution.java
+++ src/main/java/_107_binary_tree_level_order_traversal_ii/Solution.java
@@ -1,9 +1,49 @@
 
 package _107_binary_tree_level_order_traversal_ii;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/binary-tree-level-order-traversal-ii
  */
 public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        Stack<List<Integer>> listsStack = new Stack<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
 
+        while (!stack.empty()) {
+            List<Integer> list = new ArrayList<>();
+            Stack<TreeNode> levelNodes = new Stack<>();
+            // pop all element to another stack
+            while (!stack.empty()) {
+                levelNodes.push(stack.pop());
+            }
+            while (!levelNodes.empty()) {
+                TreeNode node = levelNodes.pop();
+                if(node == null) continue;
+                stack.push(node.left);
+                stack.push(node.right);
+                list.add(node.val);
+            }
+            if(!list.isEmpty()) listsStack.push(list);
+        }
+
+        while (!listsStack.empty()) {
+            lists.add(listsStack.pop());
+        }
+
+        return lists;
+    }
+}
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 }
--- src/main/java/_107_binary_tree_level_order_traversal_ii/Tests.java
+++ src/main/java/_107_binary_tree_level_order_traversal_ii/Tests.java
@@ -6,9 +6,41 @@ import org.junit.Test;
 public class Tests {
 	private Solution solution = new Solution();
 
+	//	   			3
+//			   / \
+//			   9  20
+//			     /  \
+//			    15   7
 	@Test
 	public void test1() {
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node9 = new TreeNode(9);
+		TreeNode node20 = new TreeNode(20);
+		TreeNode node15 = new TreeNode(15);
+		TreeNode node7 = new TreeNode(7);
 
+		node3.left = node9;
+		node3.right = node20;
+		node20.left = node15;
+		node20.right = node7;
+
+		System.out.println(solution.levelOrderBottom(node3));
+	}
+
+	@Test
+	public void test2() {
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+
+		node1.left = node2;
+		node1.right = node3;
+		node2.left = node4;
+		node3.right = node5;
+
+		System.out.println(solution.levelOrderBottom(node1));
 	}
 	
 }
