100fbc1db0a6ba3923795c76a9ebe425c491a310
Chao Yang <chaoyang.nz@gmail.com>
2017-01-01 11:26:08 +0800

    done

--- src/main/java/_283_move_zeroes/Solution.java
+++ src/main/java/_283_move_zeroes/Solution.java
@@ -5,5 +5,26 @@ package _283_move_zeroes;
  * https://leetcode.com/problems/move-zeroes
  */
 public class Solution {
+    public void moveZeroes(int[] nums) {
+        int k = nums.length-1;
+
+        int i = nums.length-1;
+        while (i >= 0) {
+            if(nums[i] == 0) {
+                bubble(nums, i, k);
+                k--;
+            }
+            i--;
+        }
+    }
+
+    private static void bubble(int[] nums, int from, int to) {
+        if(from == to) return;
+        for(int i = from; i < to; i++) {
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+        }
+    }
 
 }
--- src/main/java/_283_move_zeroes/Tests.java
+++ src/main/java/_283_move_zeroes/Tests.java
@@ -3,12 +3,23 @@ package _283_move_zeroes;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Tests {
 	private Solution solution = new Solution();
 
 	@Test
 	public void test1() {
+		int[] nums = {0, 1, 0, 3, 12};
+		solution.moveZeroes(nums);
+		System.out.println(Arrays.toString(nums));
+	}
 
+	@Test
+	public void test2() {
+		int[] nums = {1,0,0};
+		solution.moveZeroes(nums);
+		System.out.println(Arrays.toString(nums));
 	}
 	
 }
