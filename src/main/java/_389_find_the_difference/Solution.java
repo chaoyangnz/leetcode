b8a784dbe0888b21c598ce2d4e5df64f0c1f2cf2
Chao Yang <chaoyang.nz@gmail.com>
2017-05-05 22:07:12 +1200

    done

--- src/main/java/_389_find_the_difference/Solution.java
+++ src/main/java/_389_find_the_difference/Solution.java
@@ -5,5 +5,17 @@ package _389_find_the_difference;
  * https://leetcode.com/problems/find-the-difference
  */
 public class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        final int n = s.length();
+        int ss = 0, st = 0;
+        for(int i = 0; i < n; ++i) {
+            ss += cs[i];
+            st += ct[i];
+        }
+        st += ct[n];
 
+        return (char) (st - ss);
+    }
 }
