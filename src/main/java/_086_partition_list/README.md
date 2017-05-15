eaf8b574612d8cd6643d75c2a3ef1e034192a08c
Chao Yang <chaoyang.nz@gmail.com>
2017-05-15 15:38:29 +1200

    done

---
+++ src/main/java/_086_partition_list/README.md
@@ -0,0 +1,13 @@
+# Problem
+
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given `1->4->3->2->5->2` and x = 3,
+return `1->2->2->4->3->5`.
+
+# Solution
+
+head会改变，使用dummy head。 记录tail，逐步扩展
--- src/main/java/_086_partition_list/Solution.java
+++ src/main/java/_086_partition_list/Solution.java
@@ -1,9 +1,33 @@
 
 package _086_partition_list;
 
+import _000_util.ListNode;
+
 /**
  * https://leetcode.com/problems/partition-list
  */
 public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummyHead1 = new ListNode(0);
+        ListNode tail1 = dummyHead1;
 
+        ListNode dummyHead2 = new ListNode(0);
+        ListNode tail2 = dummyHead2;
+
+        ListNode node = head;
+        while(node != null) {
+            if(node.val < x) {
+                tail1.next = node;
+                tail1 = node;
+            } else {
+                tail2.next = node;
+                tail2 = node;
+            }
+            node = node.next;
+        }
+        tail1.next = dummyHead2.next;
+        tail2.next = null;
+
+        return dummyHead1.next;
+    }
 }
