7434b8f5964de13c07b93e65b788a4eba08ec9cf
Chao Yang <chaoyang.nz@gmail.com>
2018-03-11 18:34:39 +1300

    done

--- src/main/java/_102_binary_tree_level_order_traversal/Solution.java
+++ src/main/java/_102_binary_tree_level_order_traversal/Solution.java
@@ -11,8 +11,30 @@ import java.util.*;
  */
 public class Solution {
 
-    // improved version: use one queue but record the level.
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < num; ++i) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            lists.add(list);
+        }
+
+        return lists;
+    }
+
+    // improved version: use one queue but record the level.
+    public List<List<Integer>> levelOrder11(TreeNode root) {
         List<List<Integer>> lists = new ArrayList<>();
         Queue<Pair> thisLevel = new LinkedList<>(); // all nodes are not null
 
